f7681bb64ec01dbe2b10ee7ea6d5be7e90659f62
John Muskett <j.muskett@me.com>
2023-06-04 20:21:22 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const Beef = () => {
+  return (
+    <View>
+      <Text>Beef</Text>
+    </View>
+  );
+};
+
+const Chicken = () => {
+  return (
+    <View>
+      <Text>Chicken</Text>
+    </View>
+  );
+};
+
+const Main = ({ navigation }: any) => {
+  return (
+    <View style={{ paddingHorizontal: "10%" }}>
+      <Text>Welcome to BBQ It, the app that helps you BBQ!</Text>
+      <Text>To get started, select a meat</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("Beef")}>
+        <Text>Beef</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.text}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen name="Main" component={Main} />
+            <Stack.Screen name="Beef" component={Beef} />
+            <Stack.Screen name="Chicken" component={Chicken} />
+          </Stack.Navigator>
+        </NavigationContainer>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -13,8 +53,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+
+    ...StyleSheet.absoluteFillObject,
+  },
+  text: {
+    width: "100%",
+    flex: 3,
+    backgroundColor: "#fff",
+
+    paddingVertical: "10%",
+    justifyContent: "center",
+
+    ...StyleSheet.absoluteFillObject,
   },
 });
